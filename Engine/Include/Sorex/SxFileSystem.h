eab2e435b550abd2cbddc7aaad965431c9498930
ARuffy <aershov.ge@gmail.com>
2024-12-13 23:03:16 -0600

    [Engine] Add SxPathUtils

--- Engine/Include/Sorex/SxFileSystem.h
+++ Engine/Include/Sorex/SxFileSystem.h
@@ -30,5 +30,89 @@
 #include "CoreMinimal.h"
 #include "Stream.h"
 
+#include <filesystem>
+
 namespace Sorex
-{}
+{
+
+  // TODO: documentation: use /dir/to/file.txt format of directory. The
+  // FileSystem should format and map it
+  namespace FileSystem
+  {
+    using Path = std::filesystem::path;
+    enum class EFileStatus
+    {
+      Unknown,  ///< Unknown file doesn't exists
+      Missing,  ///< File is missing (exp. downloading)
+      Existent  ///< File exists and ready to work
+    };
+  }  // namespace
+
+  class IFileSystem
+  {
+public:
+    virtual ~IFileSystem() = default;
+
+    virtual Status IndexFiles() SRX_NOEXCEPT = 0;
+
+    /**
+     * @brief Retrieve list of all files from filesystem by the path.
+     *
+     *  If path is a directory it should provide all files in the directory;
+     *  If path is a path to the file it should provide all files with the same
+     * name:
+     *      `/path/to/file` can return `/path/to/file.xml`, `/path/to/file.png`,
+     * ...
+     *
+     * @param path - path to search;
+     * @param out files - list to store files;
+     */
+    virtual void GetFiles(StringView path, TVector<String>& files) = 0;
+
+    /**
+     * @brief Retrive status of a file.
+     *
+     * @param filename - name of the file.
+     * @return status of the file.
+     */
+    virtual FileSystem::EFileStatus GetFileStatus(StringView filename) = 0;
+
+    /**
+     * @brief Retrive the file index.
+     *
+     * @param filename - name of the file.
+     * @return index of the file.
+    virtual const FileSystem::FileIndex* GetFileIndex(StringView filename) const
+    = 0;
+     */
+
+    /**
+     * @brief Check if file exists.
+     *
+     * @param path - path to the file
+     * @return True if file exists in the file system.
+     */
+    SRX_INLINE bool IsFileExists(StringView path) SRX_NOEXCEPT
+    {
+      return GetFileStatus(path) == FileSystem::EFileStatus::Existent;
+    }
+
+    /**
+     * @brief Retrieve absolute path to the file system in the native path
+     * format.
+     *
+     * @return path to the file system.
+     */
+    virtual FileSystem::Path GetSystemPath() SRX_NOEXCEPT = 0;
+
+    /**
+     * @brief Open file.
+     *
+     * @param path
+     * @param error
+     * @return pointer to the opened file or NULL.
+     */
+    virtual TUniquePointer<Stream> OpenFile(StringView path,
+                                            Status*    status) SRX_NOEXCEPT = 0;
+  };
+}
---
+++ Engine/Source/FileSystem/SxPathUtils.h
@@ -0,0 +1,184 @@
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                                SOREX                                   */
+/*                 Simple OpenGL Rendering Engine eXtended                */
+/**************************************************************************/
+/* Copyright (c) 2022 Aleksandr Ershov (Ruffy).                           */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#include <Sorex/CoreMinimal.h>
+#include <Sorex/SxFileSystem.h>
+
+namespace Sorex::Utils
+{
+  SRX_INLINE constexpr char GetPathDelimiter() SRX_NOEXCEPT
+  {
+    return '/';
+  }
+
+  /**
+   * @brief Combine dirs to a path.
+   *
+   * @return combined path in generic format
+   */
+  String Combine(const TVector<StringView>& dirs) SRX_NOEXCEPT;
+  String Combine(const TVector<String>& dirs) SRX_NOEXCEPT;
+
+  /**
+   * @brief Ensure that path closed with slash.
+   *
+   * @return path with slash on the end
+   */
+  String EnsureClosingSlash(StringView path) SRX_NOEXCEPT;
+
+  /**
+   * @brief Retrieve extension of file.
+   *
+   * @param path - path to the file;
+   * @param bIncludeDot - include dot of the extension in result;
+   * @return extension of the file by path;
+   */
+  StringView GetFileExtension(StringView path,
+                              bool       bIncludeDot = false) SRX_NOEXCEPT;
+
+  /**
+   * @brief Split path to two parts <dirname, filename>.
+   *
+   *
+   * @param path - path for decomposing
+   * @param bClosingSlash - enable closing slash for `dirname`
+   * @return pair <dirname, filename>.
+   */
+  TPair<StringView, StringView> SplitPath(StringView path,
+                                          bool       bClosingSlash = false)
+    SRX_NOEXCEPT;
+
+  /**
+   * @brief Split path to two parts <dirname, filename>.
+   *
+   * @param path - path for decomposing
+   * @param bClosingSlash - enable closing slash for `dirname`
+   * @param out - pair of string to store <dirname, filename>
+   */
+  void SplitPath(StringView             path,
+                 TPair<String, String>& out,
+                 bool                   bClosingSlash = false) SRX_NOEXCEPT;
+
+  /**
+   * @brief Return dir (base) path.
+   *
+   * It work only with generic format (with '/' slach separator)
+   * Return directory name in format `/path/to/dir`.
+   * If bClosingSlash is True the closing slash will be included
+   * `/path/to/dir/`.
+   *
+   * @note: don't use with string literal
+   *
+   * @param path - path for decomposing
+   * @param bClosingSlash - enable closing slash
+   * @return directory name or empty string if path invalid.
+   */
+  StringView GetBaseName(StringView path,
+                         bool       bClosingSlash = false) SRX_NOEXCEPT;
+
+  /**
+   * @brief Return dir (base) path.
+   *
+   * It work only with generic format (with '/' slach separator)
+   * Return directory name in format `/path/to/dir`.
+   * If bClosingSlash is True the closing slash will be included
+   * `/path/to/dir/`.
+   *
+   * @param path - path for decomposing
+   * @param bClosingSlash - enable closing slash
+   * @param base - string that will be store the result
+   */
+  void GetBaseName(StringView path,
+                   String&    base,
+                   bool       bClosingSlash = false) SRX_NOEXCEPT;
+
+  /**
+   * @brief Return root dir (base) of a path.
+   *
+   * It work only with generic format (with '/' slach separator)
+   * Return directory name in format `/dir`
+   * Example: `/dir_name/path/to/file` -> `/dir_name`
+   * If bClosingSlash is True the closing slash will be included.
+   *
+   * @param path - path for decomposing
+   * @param bClosingSlash - enable closing slash
+   * @return name of root directory or empty string if path invalid.
+   */
+  StringView GetRootName(StringView path,
+                         bool       bClosingSlash = false) SRX_NOEXCEPT;
+
+  /**
+   * @brief Return root dir (base) of a path.
+   *
+   * It work only with generic format (with '/' slach separator)
+   * Return directory name in format `/dir`
+   * Example: `/dir_name/path/to/file` -> `/dir_name`
+   * If bClosingSlash is True the closing slash will be included.
+   *
+   * @param path - path for decomposing
+   * @param bClosingSlash - enable closing slash
+   * @param root - string to store name of root directory or empty string if
+   * path invalid.
+   */
+  SRX_INLINE void GetRootName(StringView path,
+                              String&    root,
+                              bool       bClosingSlash = false) SRX_NOEXCEPT;
+
+  // Platform implementation
+
+  /**
+   * @brief Retrieve path to writable directory of the user.
+   *
+   * @return directory path in the generic format.
+   */
+  FileSystem::Path GetUserAppsDataPath() SRX_NOEXCEPT;
+
+  /**
+   * @brief Retrieve path to folder where application was started.
+   *
+   * @return directory path in the generic format
+   */
+  FileSystem::Path GetApplicationPath() SRX_NOEXCEPT;
+}
+
+SRX_INLINE void Sorex::Utils::GetBaseName(StringView path,
+                                          String&    basename,
+                                          bool bClosingSlash /* = false */)
+  SRX_NOEXCEPT
+{
+  basename = Sorex::Utils::GetBaseName(path, bClosingSlash);
+}
+
+SRX_INLINE void Sorex::Utils::GetRootName(StringView path,
+                                          String&    root,
+                                          bool bClosingSlash /* = false */)
+  SRX_NOEXCEPT
+{
+  root = Sorex::Utils::GetRootName(path, bClosingSlash);
+}
